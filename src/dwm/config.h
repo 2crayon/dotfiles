981b786a921b8570b0eb868878caea5858ec2b82
Ely <elyalon345@gmail.com>
2024-05-31 10:43:47 +0300

    a

--- src/dwm/config.h
+++ src/dwm/config.h
@@ -105,7 +105,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_x,      killclient,     {0} },
 	{ MODKEY|ShiftMask|ControlMask, XK_q,      quit,           {0} },
 };
 
