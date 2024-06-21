9e8527f78f6bf1c0b05748ded6c6538af93d5f17
Ely <elyalon345@gmail.com>
2024-06-21 08:44:12 +0300

    a

---
+++ .config/fish/functions/pt.fish
@@ -0,0 +1,4 @@
+function pt --wraps='frk sudo -E gparted' --description 'alias pt=frk sudo -E gparted'
+  frk sudo -E gparted $argv
+        
+end
--- .config/fish/functions/rs.fish
+++
@@ -1,4 +0,0 @@
-function rs --wraps=restic --wraps='sudo restic' --wraps='sudo restic --repo /media/backup' --description 'alias rs=sudo restic --repo /media/backup'
-  sudo restic --repo /media/backup $argv
-        
-end
