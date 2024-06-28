69054dd797c4ba18cdca5f69020a8d5d789fc490
Ely <elyalon345@gmail.com>
2024-06-28 12:09:59 +0300

    a

--- .config/fish/functions/zigup.fish
+++ .config/fish/functions/zigup.fish
@@ -1,4 +1,3 @@
 function zigup --description 'alias zigup=zigup --install-dir ~/managed/zig --path-link ~/.local/bin/zig'
- command zigup --install-dir ~/managed/zig --path-link ~/.local/bin/zig $argv
-        
+    command zigup --install-dir ~/managed/zig --path-link ~/.local/bin/zig $argv
 end
